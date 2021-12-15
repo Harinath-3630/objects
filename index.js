2b1822878d0b6fa3f1223fb821756cb720f2ae88
Harinath <kathiarun3630@gmail.com>
2021-12-15 22:33:13 +0530

    objects

---
+++ index.js
@@ -0,0 +1,12 @@
+var student ={
+    name:"Ravi",
+    Age:15,
+    marks: {
+        science:75,
+        maths:90,
+        physics:80,
+    }
+}
+console.log(student.Age)
+console.log(student.marks.maths)
+console.log(typeof student)
